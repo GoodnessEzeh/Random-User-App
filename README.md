7ba158ccb1c3c68e68b5508c8cbf3619f070e860
Goodness Ezeh <ezehgoodness@gmail.com>
2022-01-09 20:05:27 +0900

    second commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# random_user app
+
+## Technologies used
+
+## To Run the project execute following commands in sequence
+
+1. npm install
+2. npm run build
+3. npm run start-server
--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import store from './store/store';
+import { addUsers } from './actions/users';
+import { Provider } from 'react-redux';
+import Header from './components/Header';
+import UsersList from './components/UsersList';
+import './css/styles.css';
 
 class App extends React.Component {
+    componentDidMount() {
+        axios.get('/users')
+            .then(response => {
+                console.log(response.data);
+                store.dispatch(addUsers(response.data.results));
+            })
+    }
     render() {
         return (
-            <h1>Welcome to React!</h1>
+            <div className="main-section">
+                <Header />
+                <UsersList />
+            </div>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+document.getElementById('root'));
